569a20fcb1b3d53fa96a6a4998a0a262bc3ef86e
Yileri <75039627+Yileri@users.noreply.github.com>
2023-02-18 19:32:22 +0300

    Update Receiver.py

--- Receiver.py
+++ Receiver.py
@@ -1,37 +1,33 @@
-"""
-Server receiver of the file
-"""
 import socket
 import tqdm
 import os
-import boto3
-
-ec2 = boto3.client("ec2", 
-                   "us-east-1", 
-                   aws_access_key_id="", #key id here
-                   aws_secret_access_key="" #access key here
-                  )
-
-
+from cryptography.fernet import Fernet
 
 # device's IP address
-SERVER_HOST = "localhost"
-SERVER_PORT = 11467 #this can change as 
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 11467
+
 # receive 4096 bytes each time
 BUFFER_SIZE = 4096
+
 SEPARATOR = "<SEPARATOR>"
+
 # create the server socket
 # TCP socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket()
+
 # bind the socket to our local address
 s.bind((SERVER_HOST, SERVER_PORT))
+
 # enabling our server to accept connections
 # 5 here is the number of unaccepted connections that
 # the system will allow before refusing new connections
 s.listen(5)
 print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
 # accept connection if there is any
-client_socket, address = s.accept()
+client_socket, address = s.accept() 
+
 # if below code is executed, that means the sender is connected
 print(f"[+] {address} is connected.")
 
@@ -39,10 +35,18 @@ print(f"[+] {address} is connected.")
 # receive using client socket, not server socket
 received = client_socket.recv(BUFFER_SIZE).decode()
 filename, filesize = received.split(SEPARATOR)
+
 # remove absolute path if there is
 filename = os.path.basename(filename)
 # convert to integer
 filesize = int(filesize)
+
+# read the key from filekey
+with open("filekey.key", "rb") as filekey:
+    key = filekey.read()
+
+# create fernet key
+fernet = Fernet(key)
 # start receiving the file from the socket
 # and writing to the file stream
 progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
@@ -50,11 +54,13 @@ with open(filename, "wb") as f:
     while True:
         # read 1024 bytes from the socket (receive)
         bytes_read = client_socket.recv(BUFFER_SIZE)
-        if not bytes_read:
+        if not bytes_read:    
             # nothing is received
             # file transmitting is done
             break
-        # write to the file the bytes we just received
+	# decrypt received encrypted data
+        bytes_read = fernet.decrypt(bytes_read)
+	# write decrypted data on file
         f.write(bytes_read)
         # update the progress bar
         progress.update(len(bytes_read))
@@ -63,4 +69,3 @@ with open(filename, "wb") as f:
 client_socket.close()
 # close the server socket
 s.close()
-
